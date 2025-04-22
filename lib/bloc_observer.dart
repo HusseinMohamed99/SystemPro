86d758c5e372264c67f7b6ff3179af858fcefd57
Hussein Mohamed <84459939+HusseinMohamed99@users.noreply.github.com>
2025-04-22 11:37:06 +0200

    chore: add rename package, update app labels, and generate asset files
    Add the 'rename' package to pubspec.yaml for file renaming functionality. Update the app label to 'System Pro' in both iOS and Android configurations. Generate asset files for fonts and add a new BlocObserver implementation for logging bloc events.

---
+++ lib/bloc_observer.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+
+import 'core/helpers/functions/app_logs.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+
+    AppLogs.successLog('onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+
+    AppLogs.infoLog('onChange -- ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    AppLogs.errorLog('onError -- ${bloc.runtimeType}, $error');
+
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+
+    AppLogs.closeLog('onClose -- ${bloc.runtimeType}');
+  }
+}
---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,62 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/Poppins-Black.ttf
+  String get poppinsBlack => 'assets/fonts/Poppins-Black.ttf';
+
+  /// File path: assets/fonts/Poppins-Bold.ttf
+  String get poppinsBold => 'assets/fonts/Poppins-Bold.ttf';
+
+  /// File path: assets/fonts/Poppins-ExtraBold.ttf
+  String get poppinsExtraBold => 'assets/fonts/Poppins-ExtraBold.ttf';
+
+  /// File path: assets/fonts/Poppins-ExtraLight.ttf
+  String get poppinsExtraLight => 'assets/fonts/Poppins-ExtraLight.ttf';
+
+  /// File path: assets/fonts/Poppins-Light.ttf
+  String get poppinsLight => 'assets/fonts/Poppins-Light.ttf';
+
+  /// File path: assets/fonts/Poppins-Medium.ttf
+  String get poppinsMedium => 'assets/fonts/Poppins-Medium.ttf';
+
+  /// File path: assets/fonts/Poppins-Regular.ttf
+  String get poppinsRegular => 'assets/fonts/Poppins-Regular.ttf';
+
+  /// File path: assets/fonts/Poppins-SemiBold.ttf
+  String get poppinsSemiBold => 'assets/fonts/Poppins-SemiBold.ttf';
+
+  /// File path: assets/fonts/Poppins-Thin.ttf
+  String get poppinsThin => 'assets/fonts/Poppins-Thin.ttf';
+
+  /// List of all assets
+  List<String> get values => [
+    poppinsBlack,
+    poppinsBold,
+    poppinsExtraBold,
+    poppinsExtraLight,
+    poppinsLight,
+    poppinsMedium,
+    poppinsRegular,
+    poppinsSemiBold,
+    poppinsThin,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const String aEnv = '.env';
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
+}
