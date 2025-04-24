173859d84f3af6fcec8ad6bd4aaf6243fde34039
Hussein Mohamed <84459939+HusseinMohamed99@users.noreply.github.com>
2025-04-24 13:01:44 +0200

    refactor(login): extract bloc consumer logic into separate widget
    Move the BlocConsumer logic from LoginView to a new widget, LoginViewBlocConsumer, to improve code maintainability and separation of concerns

---
+++ lib/features/Authentication/Login/ui/widgets/login_view_bloc_consumer.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:system_pro/core/helpers/dimensions/dimensions.dart';
+import 'package:system_pro/core/helpers/extensions/localization_extension.dart';
+import 'package:system_pro/core/helpers/extensions/snack_bar_extension.dart';
+import 'package:system_pro/core/helpers/extensions/widget_extension.dart';
+import 'package:system_pro/core/widgets/indicators/custom_loading_indicator.dart';
+import 'package:system_pro/features/Authentication/Login/logic/login_cubit.dart';
+import 'package:system_pro/features/Authentication/Login/logic/login_state.dart';
+import 'package:system_pro/features/Authentication/Login/ui/widgets/login_view_body.dart';
+
+class LoginViewBlocConsumer extends StatelessWidget {
+  const LoginViewBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginSuccess) {
+          context.showSnackBar(context.localization.sign_in_successfully);
+          // context.pushReplacementNamed(Routes.mainView);
+        }
+        if (state is LoginError) {
+          context.showSnackBar(state.error);
+        }
+      },
+      builder: (context, state) {
+        return LoadingIndicator(
+          isLoading: state is LoginLoading ? true : false,
+          child: const LoginViewBody().allPadding(
+            vPadding: kPaddingLargeVertical,
+            hPadding: kPaddingDefaultHorizontal,
+          ),
+        );
+      },
+    );
+  }
+}
