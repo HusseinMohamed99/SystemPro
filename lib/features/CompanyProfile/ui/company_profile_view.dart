36dcb7649ab9936a1b5b9e3bc6ba2ce92a50ea2c
Hussein Mohamed <84459939+HusseinMohamed99@users.noreply.github.com>
2025-05-11 11:32:55 +0300

    refactor(CompanyProfileView): remove unused imports and simplify code
    This commit removes unnecessary imports and simplifies the `CompanyProfileView` widget to improve maintainability and reduce clutter. The changes focus on removing unused dependencies and streamlining the code structure without altering the widget's behavior.

--- lib/features/CompanyProfile/ui/company_profile_view.dart
+++ lib/features/CompanyProfile/ui/company_profile_view.dart
@@ -1,27 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:system_pro/core/helpers/dimensions/dimensions.dart';
 import 'package:system_pro/core/helpers/extensions/localization_extension.dart';
-import 'package:system_pro/core/helpers/extensions/theming_extension.dart';
 import 'package:system_pro/core/helpers/extensions/widget_extension.dart';
-import 'package:system_pro/core/helpers/responsive/spacing.dart';
-import 'package:system_pro/core/theming/colorsManager/color_manager.dart';
-import 'package:system_pro/core/theming/styleManager/font_weight.dart';
 import 'package:system_pro/core/widgets/appBars/basic_app_bar.dart';
-import 'package:system_pro/core/widgets/appBars/custom_secondary_app_bar.dart';
 import 'package:system_pro/core/widgets/errors/custom_error_widget.dart';
-import 'package:system_pro/core/widgets/images/custom_cached_network_image.dart';
 import 'package:system_pro/core/widgets/indicators/custom_loading_indicator.dart';
 import 'package:system_pro/features/CompanyProfile/logic/real_estate_cubit.dart';
 import 'package:system_pro/features/CompanyProfile/logic/real_estate_state.dart';
-import 'package:system_pro/features/CompanyProfile/ui/widgets/about_real_estate_widget.dart';
 import 'package:system_pro/features/CompanyProfile/ui/widgets/get_profile_company_success.dart';
-import 'package:system_pro/features/CompanyProfile/ui/widgets/location_widget.dart';
-import 'package:system_pro/features/CompanyProfile/ui/widgets/properties_widget.dart';
-import 'package:system_pro/features/Home/data/model/company.dart';
-import 'package:system_pro/features/Home/data/model/listing.dart';
-import 'package:system_pro/features/Home/ui/real_estate_widget/real_estate_sliver_list.dart';
 
 class CompanyProfileView extends StatelessWidget {
   const CompanyProfileView({super.key, required this.companyID});
@@ -40,7 +27,6 @@ class CompanyProfileView extends StatelessWidget {
             final listings = state.filteredListings;
             final companyListings =
                 listings.where((l) => l.company?.id == companyID).toList();
-
             if (companyListings.isEmpty ||
                 companyListings.first.company == null) {
               return CustomErrorWidget(
@@ -59,4 +45,3 @@ class CompanyProfileView extends StatelessWidget {
     );
   }
 }
-
