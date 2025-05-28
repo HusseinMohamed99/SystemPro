ddf91f0811c70ad6bbbc2a95995d3bc3817ab033
Hussein Mohamed <84459939+HusseinMohamed99@users.noreply.github.com>
2025-05-28 15:20:52 +0300

    feat(widgets): add animations to HaveAnAccountWidget
    Add flutter_animate dependency and implement fade/slide animations for better user experience. The widget now animates when appearing on screen with customizable duration.

--- lib/core/widgets/texts/have_an_account.dart
+++ lib/core/widgets/texts/have_an_account.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:system_pro/core/helpers/extensions/theming_extension.dart';
 import 'package:system_pro/core/theming/colorsManager/color_manager.dart';
 import 'package:system_pro/core/theming/styleManager/font_weight.dart';
@@ -10,40 +10,48 @@ class HaveAnAccountWidget extends StatelessWidget {
     required this.title1,
     required this.title2,
     required this.onTap,
+    this.animationDuration = const Duration(milliseconds: 600),
   });
-  final String title1, title2;
+
+  final String title1;
+  final String title2;
   final VoidCallback onTap;
+  final Duration animationDuration;
 
   @override
   Widget build(BuildContext context) {
-    return Text.rich(
-      TextSpan(
-        children: [
-          TextSpan(
-            text: title1,
-            style: context.titleMedium?.copyWith(
-              color: AdaptiveColor.adaptiveColor(
-                context: context,
-                lightColor: ColorManager.softGray,
-                darkColor: ColorManager.pureWhite,
-              ),
-            ),
-          ),
-          TextSpan(
-            text: ' $title2',
-            style: context.titleMedium?.copyWith(
-              color: AdaptiveColor.adaptiveColor(
-                context: context,
-                lightColor: ColorManager.primaryBlue,
-                darkColor: ColorManager.secondaryBlue,
-              ),
-              fontWeight: FontWeightHelper.semiBold,
-            ),
-            recognizer: TapGestureRecognizer()..onTap = onTap,
-          ),
-        ],
+    final defaultStyle = context.titleMedium?.copyWith(
+      color: AdaptiveColor.adaptiveColor(
+        context: context,
+        lightColor: ColorManager.softGray,
+        darkColor: ColorManager.pureWhite,
       ),
-      textAlign: TextAlign.center,
+    );
+
+    final highlightStyle = context.titleMedium?.copyWith(
+      color: AdaptiveColor.adaptiveColor(
+        context: context,
+        lightColor: ColorManager.primaryBlue,
+        darkColor: ColorManager.secondaryBlue,
+      ),
+      fontWeight: FontWeightHelper.semiBold,
+    );
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(text: title1, style: defaultStyle),
+                TextSpan(text: ' $title2', style: highlightStyle),
+              ],
+            ),
+          )
+          .animate()
+          .fadeIn(duration: animationDuration)
+          .slideY(begin: 0.3, end: 0.0, duration: animationDuration),
     );
   }
 }
